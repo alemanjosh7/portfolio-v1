c54717b32852791353b653f2c2a663a14f88e5d4
alemanjosh7 <josuealeman2003@gmail.com>
2021-06-08 19:54:46 -0600

    making some changes

--- js/app.js
+++ js/app.js
@@ -2,3 +2,4 @@ var about = new SmoothScroll('a[href*="#about"]');
 var education = new SmoothScroll('a[href*="#education"]');
 var skills = new SmoothScroll('a[href*="#skills"]');
 var references = new SmoothScroll('a[href*="#references"]');
+//test
